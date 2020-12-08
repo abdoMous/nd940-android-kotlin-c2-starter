d49f1b2108573ac05d317b9222a86f7b572e2cb3
Abderrahman MOUSSAOUI <contact@moussaoui91.com>
2020-12-08 12:37:36 +0100

    Include Main screen with a list of clickable asteroids

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -2,8 +2,11 @@ package com.udacity.asteroidradar.main
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
@@ -21,6 +24,24 @@ class MainFragment : Fragment() {
         binding.viewModel = viewModel
 
         setHasOptionsMenu(true)
+
+        val adapter = AsteroidAdapter(AsteroidListener {
+            asteroidId -> viewModel.onAsteroidClicked(asteroidId)
+        })
+        binding.asteroidRecycler.adapter = adapter
+
+        viewModel.asteroids.observe(viewLifecycleOwner, Observer {
+            it?.let{
+                adapter.submitList(it)
+            }
+        })
+
+        viewModel.navigateToDetail.observe(viewLifecycleOwner, Observer { asteroid ->
+            asteroid?.let {
+                findNavController().navigate(MainFragmentDirections.actionShowDetail(asteroid))
+                viewModel.onDetailNavigated()
+            }
+        })
 
         return binding.root
     }
--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,37 @@
 package com.udacity.asteroidradar.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.udacity.asteroidradar.Asteroid
 
 class MainViewModel : ViewModel() {
+
+    private val _navigateToDetail = MutableLiveData<Asteroid>()
+    val navigateToDetail : LiveData<Asteroid>
+        get() = _navigateToDetail
+
+    fun onAsteroidClicked(asteroid: Asteroid) {
+        _navigateToDetail.value = asteroid
+    }
+
+    fun onDetailNavigated(){
+        _navigateToDetail.value = null
+    }
+
+    private val _asteroids = MutableLiveData<List<Asteroid>>()
+    val asteroids: LiveData<List<Asteroid>>
+        get() = _asteroids
+
+    init {
+        _asteroids.value = listOf(Asteroid(2465633, "465633 (2009 JR5)",
+            "2015-09-08", 20.3, 0.23,
+            8.12,0.30, true),
+            Asteroid(222222, "222 (2 JR5)",
+                "2002-02-02", 20.3, 0.23,
+                8.12,0.30, false),
+            Asteroid(33333, "3333 (33 JR5)",
+                "2003-03-03", 20.3, 0.23,
+                8.12,0.30, true))
+    }
 }
